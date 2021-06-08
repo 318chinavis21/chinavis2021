cdf80877388e689b2e52e7219a6a9d8a4b34c7f8
EchoChou990919 <1505682793@qq.com>
2021-06-08 20:57:46 +0800

    传输路径和雷达图

---
+++ src/components/radarChart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div style="width:500px; height:500px"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: "radarChart",
+  computed: {
+    options: function (){
+      return {
+        title: {
+          text: '污染物质雷达图',
+          top: '10px',
+          left: '10px'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        radar: {
+          // shape: 'circle',
+          indicator: [
+            {name: 'AQI', max: 300},
+            {name: 'PM2.5', max: 250},
+            {name: 'PM10', max: 300},
+            {name: 'CO', max: 5},
+            {name: 'NO2', max: 200},
+            {name: 'SO2', max: 100},
+            {name: 'O3', max: 100}
+          ],
+        },
+        series: [{
+          name: 'XX空气质量',
+          type: 'radar',
+          areaStyle: {},
+          data: [
+            {
+              value: [200, 150, 200, 3, 160, 80, 60]
+            }
+          ]
+        }]
+      }
+    }
+  },
+  mounted() {
+    this.chart = echarts.init(this.$el);
+    this.chart.setOption(this.options);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="about">
-    <lineChart />
+    <!--lineChart /-->
+    <radarChart />
+    <!--transPath /-->
   </div>
 </template>
 
 <script>
 import lineChart from "@/components/lineChart";
+import transPath from "@/components/transPath";
+import radarChart from "@/components/radarChart";
 
 export default {
   name: "About",
 
   components: {
     lineChart,
+    transPath,
+    radarChart
   },
 };
 </script>
